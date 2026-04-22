65a04de7927c16063e49ef3fb96aebb0f7780abf
FelipeUCC898 <felipe.narvaezg@campusucc.edu.co>
2026-04-22 09:55:57 -0500

    feat: agregar barrel export para tipos del proyecto

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Barrel export para todos los tipos del proyecto
+ */
+
+export type { ExperienceEntry } from "./experience";
+export type { ProjectItem } from "./project";
+export type { Locale, SectionId } from "./section";
